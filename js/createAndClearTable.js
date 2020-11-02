c7469444a39f8c6159f66d01769fe9683dde483b
TadjievCodes <muhammad2008.08@mail.ru>
2020-11-02 18:45:51 +0500

    EDITED CrateandclearTable.js

--- js/createAndClearTable.js
+++ js/createAndClearTable.js
@@ -1,17 +1,14 @@
 let rows = 20;
-let columns  = 10;
+let columns = 10;
 
-
-function clearSpreadsheet(){
+function clearSpreadsheet() {
     createSpreadsheet();
     document.getElementById('value').value = " ";
-    for (var i = 0; i < TBLROWS; i++){
-    tableArray[i] = [];
-        for (var j = 0; j < TBLCOLUMNS; j++)
-    tableArray[i][j] = "";
+    for (var i = 0; i < TBLROWS; i++) {
+        tableArray[i] = [];
+        for (var j = 0; j < TBLCOLUMNS; j++) tableArray[i][j] = "";
     }
     console.log(tableArray);
-
 }
 
 function createSpreadsheet() {
@@ -21,21 +18,19 @@ function createSpreadsheet() {
 
 function buildTable(rows, columns) {
     var divHTML = "<table border='1' cellpadding='0' cellspacing='0' class='TableClass'>";
-            divHTML += "<tr><th></th>";
-    
-            for (var j = 0; j < columns; j++)
-                divHTML += "<th>" + String.fromCharCode(j + 65) + "</th>";
-            divHTML += "</tr>";
-    
-            for (var i = 1; i <= rows; i++) {
-                divHTML += "<tr>";
-                divHTML += "<td id='" + i + "_0' class='BaseColumn'>" + i + "</td>";
+    divHTML += "<tr><th></th>";
 
-                for (var j = 1; j <= columns; j++)
-                    divHTML += "<td id='" + i + "_" + j + "' class='AlphaColumn' onclick='clickCell(this)'></td>";
+    for (var j = 0; j < columns; j++) divHTML += "<th>" + String.fromCharCode(j + 65) + "</th>";
+    divHTML += "</tr>";
 
-                divHTML += "</tr>";
-            }
-            divHTML += "</table>";
-            return divHTML; 
+    for (var i = 1; i <= rows; i++) {
+        divHTML += "<tr>";
+        divHTML += "<td id='" + i + "_0' class='BaseColumn'>" + i + "</td>";
+
+        for (var j = 1; j <= columns; j++) divHTML += "<td id='" + i + "_" + j + "' class='AlphaColumn' onclick='clickCell(this)'></td>";
+
+        divHTML += "</tr>";
+    }
+    divHTML += "</table>";
+    return divHTML;
 }
