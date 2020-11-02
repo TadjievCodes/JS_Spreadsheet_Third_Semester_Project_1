aa50992af6a60c20556c2b79d08ac6b4a4f29593
TadjievCodes <muhammad2008.08@mail.ru>
2020-11-02 19:43:49 +0500

    changes

--- js/createAndClearTable.js
+++ js/createAndClearTable.js
@@ -4,30 +4,30 @@ let columns = 10;
 function clearSpreadsheetExcel() {
     createSpreadsheetExcel();
     document.getElementById('value').value = " ";
-    for (var i = 0; i < TBLROWS; i++) {
+    for (let i = 0; i < TBLROWS; i++) {
         tableArray[i] = [];
-        for (var j = 0; j < TBLCOLUMNS; j++) tableArray[i][j] = "";
+        for (let j = 0; j < TBLCOLUMNS; j++) tableArray[i][j] = "";
     }
     console.log(tableArray);
 }
 
 function createSpreadsheetExcel() {
-    var columns = 10;
+    let columns = 10;
     document.getElementById("SpreadsheetTable").innerHTML = buildingTable(rows, columns);
 }
 
 function buildingTable(rows, columns) {
-    var divHTMLOutput = "<table border='1' cellpadding='0' cellspacing='0' class='TableClass'>";
+    let divHTMLOutput = "<table border='1' cellpadding='0' cellspacing='0' class='TableClass'>";
     divHTMLOutput += "<tr><th></th>";
 
-    for (var j = 0; j < columns; j++) divHTMLOutput += "<th>" + String.fromCharCode(j + 65) + "</th>";
+    for (let j = 0; j < columns; j++) divHTMLOutput += "<th>" + String.fromCharCode(j + 65) + "</th>";
     divHTMLOutput += "</tr>";
 
-    for (var i = 1; i <= rows; i++) {
+    for (let i = 1; i <= rows; i++) {
         divHTMLOutput += "<tr>";
         divHTMLOutput += "<td id='" + i + "_0' class='BaseColumn'>" + i + "</td>";
 
-        for (var j = 1; j <= columns; j++) divHTMLOutput += "<td id='" + i + "_" + j + "' class='AlphaColumn' onclick='clickCell(this)'></td>";
+        for (let j = 1; j <= columns; j++) divHTMLOutput += "<td id='" + i + "_" + j + "' class='AlphaColumn' onclick='clickCells(this)'></td>";
 
         divHTMLOutput += "</tr>";
     }
--- js/formulasTable.js
+++ js/formulasTable.js
@@ -1,33 +1,33 @@
 //Create an aray global array
 let tableArray = [];
 
-//Create global variables to store rows and columns info
+//Create global letiables to store rows and columns info
 let GLOBALROW;
 let GLOBALCOLLUMN;
 
 const TBLROWS = 20;
 const TBLCOLUMNS = 10;
 //create a 2D array 
-for (var i = 0; i < TBLROWS; i++) {
+for (let i = 0; i < TBLROWS; i++) {
     tableArray[i] = [];
-    for (var j = 0; j < TBLCOLUMNS; j++) tableArray[i][j] = "";
+    for (let j = 0; j < TBLCOLUMNS; j++) tableArray[i][j] = "";
 }
 console.log(tableArray);
 
 // Formula to split a string into an array of substrings
 function getFormula(tbValue) {
-    var pattern = /[:|\(|\)]/;
+    let pattern = /[:|\(|\)]/;
     // The split() method is used to split a string into an array of substrings, and returns the new array.
-    var ar = tbValue.split(pattern);
-    var sum = ar[0].toUpperCase();
+    let ar = tbValue.split(pattern);
+    let sum = ar[0].toUpperCase();
 
     if (ar.length < 3) return null;else if (sum !== "=SUM") return null;else return ar;
 }
 
 // recalculateValues values
 function recalculateValues() {
-    for (var i = 0; i < TBLROWS; i++) {
-        for (var j = 0; j < TBLCOLUMNS; j++) {
+    for (let i = 0; i < TBLROWS; i++) {
+        for (let j = 0; j < TBLCOLUMNS; j++) {
             // check to see if table element is a formula
             if (tableArray[i][j].indexOf("=SUM") !== -1) {
                 calculateCell(i, j);
@@ -36,45 +36,45 @@ function recalculateValues() {
     }
 }
 
-// function calculate
+// function calculateCell
 function calculateCell(row, column) {
-    var tokenArray = getFormula(tableArray[row][column]);
+    let tokenArray = getFormula(tableArray[row][column]);
 
     if (tokenArray !== null) {
-        var fromColumn = tokenArray[1].substr(0, 1);
-        var fromRow = tokenArray[1].substr(1, tokenArray[1].length - 1);
+        let fromColumn = tokenArray[1].substr(0, 1);
+        let fromRow = tokenArray[1].substr(1, tokenArray[1].length - 1);
 
-        var toColumn = tokenArray[2].substr(0, 1);
-        var toRow = tokenArray[2].substr(1, tokenArray[2].length - 1);
+        let toColumn = tokenArray[2].substr(0, 1);
+        let toRow = tokenArray[2].substr(1, tokenArray[2].length - 1);
 
-        var fromRowIndex = parseFloat(fromRow) - 1;
-        var fromColIndex = fromColumn.charCodeAt(0) - 65;
+        let fromRowIndex = parseFloat(fromRow) - 1;
+        let fromColIndex = fromColumn.charCodeAt(0) - 65;
 
-        var toRowIndex = parseFloat(toRow) - 1;
-        var toColIndex = toColumn.charCodeAt(0) - 65;
+        let toRowIndex = parseFloat(toRow) - 1;
+        let toColIndex = toColumn.charCodeAt(0) - 65;
 
-        var sumTotal = 0;
+        let sumTotal = 0;
 
-        for (var i = fromRowIndex; i <= toRowIndex; i++) {
-            for (var j = fromColIndex; j <= toColIndex; j++) {
+        for (let i = fromRowIndex; i <= toRowIndex; i++) {
+            for (let j = fromColIndex; j <= toColIndex; j++) {
                 if (isFloat(tableArray[i][j])) sumTotal += parseFloat(tableArray[i][j]);
             }
         }
 
-        var cellID = row + 1 + "_" + (column + 1);
-        var ref = document.getElementById(cellID);
+        let cellID = row + 1 + "_" + (column + 1);
+        let ref = document.getElementById(cellID);
         ref.innerHTML = sumTotal;
     }
 }
 
 // is float
 function isFloat(s) {
-    var ch = "";
-    var justFloat = "0123456789.";
+    let ch = "";
+    let justFloat = "0123456789.";
     if (s.length == 0) {
         return false;
     }
-    for (var i = 0; i < s.length; i++) {
+    for (let i = 0; i < s.length; i++) {
         ch = s.substr(i, 1);
 
         if (justFloat.indexOf(ch) == -1) return false;
